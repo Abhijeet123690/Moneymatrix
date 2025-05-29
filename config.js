da2dcf9338dc652db07b16dad1812cec58f67421
Abhijeet123690 <Abhijeetkumar123690@gmail.com>
2025-05-30 00:09:57 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// Store your API keys and configuration
+const config = {
+  ALPHA_VANTAGE_API_KEY: 'YOUR_API_KEY',
+};
+
+// Don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = config;
+}
